fcef72fd4d601bffc0bc3110bec8d2d1ec705027
Yuriy Lyeshchenko <yuriylsh@live.com>
2018-11-03 12:29:09 -0500

    switching to create-react-app 2.1.1 from https://github.com/wmonk/create-react-app-typescript

--- ClientApp/src/components/Root.tsx
+++ ClientApp/src/components/Root.tsx
@@ -4,4 +4,4 @@ import { Link } from 'react-router-dom';
 
 export const Root: React.SFC = () => <div>This is root. Current environment is {process.env.NODE_ENV}. Go to <Link to="/child1">Child1</Link> or <Link to="child2/123">Child2</Link>.</div>
 export const Child1: React.SFC = () => <div>This is Child1. Back to <Link to="/">root</Link>.</div>
-export const Child2: React.SFC<RouteComponentProps<{param1: number}>> = (props) => <div>{`This is Child2 with param1 = ${props.match.params.param1}`}. Back to <Link to="/">root</Link>.</div>
+export const Child2: React.SFC<RouteComponentProps<{param1: string}>> = (props) => <div>{`This is Child2 with param1 = ${props.match.params.param1}`}. Back to <Link to="/">root</Link>.</div>
